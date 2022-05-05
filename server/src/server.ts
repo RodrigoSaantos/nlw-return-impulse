e64f678afc76e2d28eeb1518331ed9fd1cb579e2
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-05 00:27:09 -0300

    Config prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (req, res) => {
+  return res.send('hello world');
+})
+
 app.listen(3333, () => {
   console.log('HTTP server listening on port 3333 ✨')
 })
