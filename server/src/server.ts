bf7db522549ea15f70adf8e595a517c1e01520bc
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-05 00:54:17 -0300

    Config prismaClient

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json())
 
-app.get('/users', (req, res) => {
+app.post('/feedbacks', (req, res) => {
+  console.log(req.body);
   return res.send('hello world');
 })
 
