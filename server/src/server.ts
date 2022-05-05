849bfa33d874469fcd899d976c9d0f524aabe119
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-05 09:02:49 -0300

    Config nodemailer and send mail

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,42 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
 app.use(express.json())
 
-app.post('/feedbacks', (req, res) => {
-  console.log(req.body);
-  return res.send('hello world');
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "817144928fc52d",
+    pass: "09a5937dc99064"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Rodrigo Santos <contato.rodrigosaantos@gmail.com>',
+    subject: 'Novo Feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111>`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
+  })
+  return res.status(201).json({ data: feedback });
 })
 
 app.listen(3333, () => {
