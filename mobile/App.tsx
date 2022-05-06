751dbbd95f1a07e6f4a53ab15e28f1368c570dfc
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-06 09:35:28 -0300

    Custom fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,18 @@
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Widget } from './src/componentes/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium,
+  });
+
+  if (!fontsLoaded) <AppLoading/>;
+  
   return (
     <View style={{
       flex: 1,
