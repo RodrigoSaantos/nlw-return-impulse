a721ac8b02d8f77c849ebb678ae5dd357c3fad0a
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-06 09:52:03 -0300

    Floating button

--- mobile/src/componentes/Widget/index.tsx
+++ mobile/src/componentes/Widget/index.tsx
@@ -1,12 +1,20 @@
+import { ChatTeardropDots } from 'phosphor-react-native';
 import React from 'react';
-import { Text, View } from 'react-native';
-
+import { TouchableOpacity } from 'react-native';
+import { theme } from '../../theme';
 import { styles } from './styles';
+
 
 export function Widget() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: '#fff'}}>Hello world</Text>
-    </View>
+    <>
+      <TouchableOpacity style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+    </>
   );
 }
