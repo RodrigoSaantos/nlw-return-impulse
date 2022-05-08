c4f6c62c969c2af2c49ebd3b14dade3df76f869b
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-08 12:27:54 -0300

    Gesture with BottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,10 @@
+import 'react-native-gesture-handler';
 import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading'
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
-import { Widget } from './src/componentes/Widget';
+import Widget from './src/componentes/Widget';
 import { theme } from './src/theme';
 
 export default function App() {
@@ -18,12 +19,12 @@ export default function App() {
       flex: 1,
       backgroundColor: theme.colors.background,
     }}>
-      <Widget />
       <StatusBar 
         style="light" 
         backgroundColor='transparent'
         translucent
       />
+      <Widget />
     </View>
   );
 }
--- mobile/src/componentes/Widget/index.tsx
+++ mobile/src/componentes/Widget/index.tsx
@@ -1,20 +1,41 @@
 import { ChatTeardropDots } from 'phosphor-react-native';
-import React from 'react';
+import React, { useRef } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { theme } from '../../theme';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { styles } from './styles';
 
 
-export function Widget() {
+function Widget() {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={handleOpen}
+      >
         <ChatTeardropDots
           size={24}
           weight="bold"
           color={theme.colors.text_on_brand_color}
         />
       </TouchableOpacity>
+
+      <BottomSheet 
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
